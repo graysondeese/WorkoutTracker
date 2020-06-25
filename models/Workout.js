8a2b83cf374d20589af2c3c73faf71827d3d8437
Grayson Deese <gldeese@ncsu.edu>
2020-06-24 20:26:40 -0400

    bug fix in schema and api routes

--- models/Workout.js
+++ models/Workout.js
@@ -1,50 +1,27 @@
-// setting up required vars
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-// Creating schema
-
 const WorkoutSchema = new Schema(
   {
-    day: {
-      type: Date,
-      default: Date.now,
-    },
-    excercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-          required: "Please select a type of workout.",
-        },
-        name: {
-          type: String,
-          trim: true,
-          required: "Please name your workout.",
-        },
-        duration: { type: Number },
-        weight: { type: Number },
-        reps: { type: Number },
-        sets: { type: Number },
-        duration: { type: Number },
-      },
-    ],
+    day: { type: Date, default: Date.now },
+    exercises: [
+      { 
+      type: { type: String, required: "Please include a type of exercise!" },
+      name: { type: String, required: "Please include a name!" },
+      duration: { type: Number, required: "Please input a duration" },
+      distance: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number } }
+    ]
   },
   {
-    toJSON: {
-      virtuals: true,
-    },
+    toJSON: { virtuals: true }
   }
 );
-
-// Defining virtual to calc time
 WorkoutSchema.virtual("totalDuration").get(function () {
   return this.exercises.reduce((total, current) => {
     return total + current.duration;
   }, 0);
 });
-
-// creating the model we made above
 const Workout = mongoose.model("Workout", WorkoutSchema);
-// Exporting the workout model
 module.exports = Workout;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,26 +1,30 @@
-let db = require("../models");
-// const { Workout } = require("../models")
+// requring proper models
+const db = require("../models");
+const { Workout } = require("../models");
 
+// exporting all my routes
 module.exports = function (app) {
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({})
-        .then(dbWorkouts => {
-            res.json(dbWorkouts);
-        })
-        .catch(err => {
-            res.json(err);
-        })
-    })
-    app.post("/api/workouts",  ({body}, res) => {
-        // const workout = new Workout(req.body)
-        db.Workout.create(body)
+            .then(Workout => {
+                res.json(Workout);
+            })
+            .catch(err => {
+                res.json(err);
+              });
+    });
+    
+    app.post("/api/workouts", (req,res) => {
+      const workout = new Workout(req.body)
+      db.Workout.create(workout)
         .then(Workout => {
-            res.json(Workout);
+          res.json(Workout);
         })
         .catch(err => {
-            res.json(err);
-        })
-    })
+          res.json(err);
+        });
+    });
+
     app.put("/api/workouts/:id", (req, res) => {
         Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body}})
             .then(workout => {
@@ -30,6 +34,7 @@ module.exports = function (app) {
                 res.json(err)
             });
     });
+
     app.get("/api/workouts/range", (req, res) => {
         db.Workout.find({})
         .then(Workout => {
@@ -39,5 +44,4 @@ module.exports = function (app) {
             res.json(err);
           });
       });
-    
-}
+};
